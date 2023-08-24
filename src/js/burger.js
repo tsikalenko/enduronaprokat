4f6f9be0b3323cee9715ec73fb55a63bfa17a1e2
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2023-08-24 10:43:39 +0300

    Update 2023-08-24T07:43:38.744Z

--- src/js/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const header = document.querySelector('.header');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('burger--cross');
-    header.classList.toggle('header--open')
-})
